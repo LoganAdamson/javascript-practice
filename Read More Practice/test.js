4f9baface4d89da5381d6e9efc9d6917a5fcc9e9
Logan Adamson <39895654+LoganAdamson@users.noreply.github.com>
2018-06-25 12:53:18 -0600

    Updated test.js
    Added onload event.

--- Read More Practice/test.js
+++ Read More Practice/test.js
@@ -1,17 +1,26 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
 
-button.onclick = function(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function(){
+    
+        if(content.className == "open"){
+            //shrink box
+            content.className = "";
+            button.innerHTML = "Show More";
+        } else{
+            //expand box
+            content.className = "open";
+            //change text
+            button.innerHTML = "Show Less";
+        }
+    };
+}
 
-    if(content.className == "open"){
-        //shrink box
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else{
-        //expand box
-        content.className = "open";
-        //change text
-        button.innerHTML = "Show Less";
-    }
+
+window.onload = function(){
+
+    setUpEvents();
 
 };
